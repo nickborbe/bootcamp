f2946586830b6c056e172967b80cff27ad1e2bf1
Nick Borbe <nickborbe@gmail.com>
2016-01-18 16:33:10 -0500

    got rid of deleted app file in lexiconomitron

--- week_2/monday/lexiconomitron/app (2).rb
+++
@@ -1,5 +0,0 @@
-
-require_relative("lib/Lexiconomitron.rb")
-lexi=Lexiconomitron.new
-#p lexi.eat_t("removes every letter t from the input")
-p lexi.oompa_loompa(["there","wat","at","bit","differential"])
