84293067d86c038e7fe04ff2b9745bcf5b97052c
Nick Borbe <nickborbe@gmail.com>
2016-01-18 16:30:22 -0500

    renamed the lexiconomitron app file

---
+++ week_2/monday/lexiconomitron/app.rb
@@ -0,0 +1,5 @@
+#I added this comment for git
+require_relative("lib/Lexiconomitron.rb")
+lexi=Lexiconomitron.new
+#p lexi.eat_t("removes every letter t from the input")
+p lexi.oompa_loompa(["there","wat","at","bit","differential"])
